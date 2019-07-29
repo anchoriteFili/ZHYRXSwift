e6a4099cf23b482b6cffc36c84b19b95a5616576
Arthur <feiliyaailiya1@icloud.com>
2019-07-29 16:18:28 +0800

    提交一下

---
+++ ZHYRXSwiftDemo/ZHYRXSwiftDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZHYRXSwiftDemo
+//
+//  Created by 赵宏亚 on 2019/7/29.
+//  Copyright © 2019 赵宏亚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
